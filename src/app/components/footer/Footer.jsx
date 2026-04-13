24da34fd2659d43fee4d74ef8c4efd433da0d5e0
Saheed <saheedsh2@gmail.com>
2026-04-13 18:29:25 +0100

    Created Home Page

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
+
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <div>Copyrights All Rights Reserved 2026</div>
-        <div></div>
+        <div className={styles.social}>
+            <Image src="/1.png" className={styles.icon} width={15} height={15} alt='facebook' />
+            <Image src="/2.png" className={styles.icon} width={15} height={15} alt='instagram' />
+            <Image src="/3.png" className={styles.icon} width={15} height={15} alt='twitter' />
+            <Image src="/4.png" className={styles.icon} width={15} height={15} alt='youtube' />
+        </div>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import logo from '../../public/dva.png'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better Design for digital Products</h1>
+        <p className={styles.desc}>
+          Transforming your ideas into reality, whether independently or collaboratively.
+        </p>
+        <button className={styles.button}>See Some Projects</button>
+      </div>
+      <div className={styles.item}>
+      <Image src={logo} alt='logo'className={styles.img} />
+      </div>
     </div>
   )
 }
