ab82e825576f4b9aeb4bd28a30c9b1045ae046f8
Saheed <saheedsh2@gmail.com>
2026-04-18 10:46:12 +0100

    Completed the blogid page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.info}>
+            <h1 className={styles.title}>
+              Lorem ipsum dolor sit
+            </h1>
+            <p className={styles.desc}>
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+            </p>
+            <div className={styles.author}>
+              <Image
+              src='/profile.jpeg'
+              width={40}
+              height={40}
+              className={styles.avatar}
+               />
+               <span className={styles.username}>John Doe</span>
+             
+            </div>
+             
+          </div>
+          
+          <div className={styles.imageContainer}>
+            <Image
+            src='/cpn.png'
+            fill={true}
+            className={styles.image}
+            
+            />
+          </div>
+
+
+        </div>
+        <div className={styles.content}>
+          <p className={styles.text}>
+             <p className={styles.desc}>
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+
+              <br />
+              <br />
+               The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+              The Quick Brown Fox Jumps Over The Lazy Dog
+            </p>
+          </p>
+        </div>
+
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/designs.webp' width={400} height={500} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Description</p>
+      </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/designs.webp' width={400} height={500} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Description</p>
+      </div>
+      </Link>
+    </div>
   )
 }
 
